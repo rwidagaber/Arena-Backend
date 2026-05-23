accd8b8ef647ea33256a7077f09afc9367e0c49a
rwida <rwidajaber@gmail.com>
2026-05-24 01:13:37 +0300

    Add DTOs for QR code generation and scanning

---
+++ ArenaBackend/ArenaApplication/Dtos/QrCode/GenerateQrCodeDto.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaApplication.Dtos.QrCode
+{
+    public class GenerateQrCodeDto
+    {
+        public Guid BookingId { get; set; }
+
+        public DateTime ExpirationTime { get; set; }
+    }
+}
