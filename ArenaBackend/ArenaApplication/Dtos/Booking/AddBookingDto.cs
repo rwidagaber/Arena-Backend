8ee31a1e918a4c2fd4132f79941e478f13eae0b6
mohammed eldeeb <mohammedeldeeb550@gmail.com>
2026-05-23 22:51:07 +0300

    test

---
+++ ArenaBackend/ArenaApplication/Dtos/Booking/AddBookingDto.cs
@@ -0,0 +1,15 @@
+﻿using ArenaDomain.Entities;
+using ArenaDomain.Entities.Bookings;
+using ArenaDomain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaApplication.Dtos.Booking
+{
+    public class AddBookingDto
+    {
+
+    
+    }
+}
