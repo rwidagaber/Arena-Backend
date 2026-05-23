8b5eef4be9350012f38cddfd7e661eb5fa70d369
rwida <rwidajaber@gmail.com>
2026-05-24 01:08:46 +0300

    Add DTOs for notification creation and representation

---
+++ ArenaBackend/ArenaApplication/Dtos/Notification/NotificationDto.cs
@@ -0,0 +1,18 @@
+﻿using ArenaDomain.Enums;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaApplication.Dtos.Notification
+{
+    internal class NotificationDto
+    {
+        public Guid Id { get; set; }
+
+        public string Title { get; set; } = string.Empty;
+
+        public string Message { get; set; } = string.Empty;
+
+        public NotificationType Type { get; set; }
+    }
+}
