af01a63de0a45c2ccd7b37c2dce9c9f94aa062c8
Wael Al-Bakry <waelbk96@gmail.com>
2026-05-24 18:32:47 +0300

    Added WorkoutExerciseDto and WorkoutDayDto Updated ExerciseDto

---
+++ ArenaBackend/ArenaApplication/Dtos/WorkoutDtos/WorkoutDayDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaApplication.Dtos.WorkoutDtos
+{
+    public class WorkoutDayDto
+    {
+        public Guid Id { get; set; }
+
+        public Guid WorkoutPlanId { get; set; }
+
+        public int DayNumber { get; set; }
+
+        public string DayName { get; set; } = string.Empty;
+
+        public List<WorkoutExerciseDto> Exercises { get; set; } = new();
+    }
+}
