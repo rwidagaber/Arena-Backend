4f74f8fdb8238634a7d790a7a4f24dae98b8e281
Nada Osama <enada1349@gmail.com>
2026-05-24 14:38:51 +0300

    Add RegisterDto

---
+++ ArenaBackend/ArenaApplication/Dtos/RegisterDto/RegisterDto.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaApplication.Dtos.RegisterDto
+{
+    public class RegisterDto
+    {
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string Password { get; set; } = null!;
+        public string ConfirmPassword { get; set; } = null!;
+
+         
+    }
+}
