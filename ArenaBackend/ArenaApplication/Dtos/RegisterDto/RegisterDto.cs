930364e8db1e7fb1875986516bb8508ebfc814aa
Nada Osama <enada1349@gmail.com>
2026-05-24 15:09:56 +0300

    Add profileDto

--- ArenaBackend/ArenaApplication/Dtos/RegisterDto/RegisterDto.cs
+++ ArenaBackend/ArenaApplication/Dtos/RegisterDto/RegisterDto.cs
@@ -12,7 +12,7 @@ namespace ArenaApplication.Dtos.RegisterDto
         public string Password { get; set; } = null!;
         public string ConfirmPassword { get; set; } = null!;
 
-        public int PhoneNumber { get; set; } = 0!;
+        public string PhoneNumber { get; set; } = null!;
         public DateOnly Birthday { get; set; }
 
 
