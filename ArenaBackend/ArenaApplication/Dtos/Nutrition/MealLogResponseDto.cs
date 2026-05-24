c2853a0b09a653ec1c39acdb4827be8d381615d9
Hajar Ismail <hajarismail2030@gmail.com>
2026-05-24 10:18:58 +0200

    add nutrition Dtos

---
+++ ArenaBackend/ArenaApplication/Dtos/Nutrition/MealLogResponseDto.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaApplication.Dtos.Nutrition
+{
+    internal class CreateMealLogDto
+    {
+    }
+}
---
+++ ArenaBackend/ArenaApplication/Dtos/Nutrition/NutritionPlanResponseDto.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaApplication.Dtos.Nutrition
+{
+    public class NutritionPlanResponseDto
+    {
+        public Guid Id { get; set; }
+        public decimal DailyCalories { get; set; }
+        public decimal ProteinGrams { get; set; }
+        public decimal CarbsGrams { get; set; }
+        public decimal FatGrams { get; set; }
+        public bool IsActive { get; set; }
+        public string AIResponseSummary { get; set; } = string.Empty;
+        public DateTime GeneratedAt { get; set; }
+        public List<MealResponseDto> Meals { get; set; } = [];
+    }
+}
