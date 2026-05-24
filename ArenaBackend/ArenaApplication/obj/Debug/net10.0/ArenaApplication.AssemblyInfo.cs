56c84e947b0e2a56b1817267d94b79faac4224df
mohammed eldeeb <mohammedeldeeb550@gmail.com>
2026-05-24 03:09:46 +0300

    Add Booking and workoutplan Dtos

--- ArenaBackend/ArenaApplication/obj/Debug/net10.0/ArenaApplication.AssemblyInfo.cs
+++ ArenaBackend/ArenaApplication/obj/Debug/net10.0/ArenaApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArenaApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22803f0f6835c6c2a019e988296ed46b2cc2ed53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee31a1e918a4c2fd4132f79941e478f13eae0b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArenaApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArenaApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
