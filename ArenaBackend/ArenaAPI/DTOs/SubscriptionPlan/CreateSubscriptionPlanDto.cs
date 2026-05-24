cc2f29c6a46b46aa65d77f14a3067991802bcf84
khaled ahmed <khaled.pro.963@gmail.com>
2026-05-24 07:30:29 +0300

    add subscription, payment, and usersubscription DTOs

---
+++ ArenaBackend/ArenaAPI/DTOs/SubscriptionPlan/CreateSubscriptionPlanDto.cs
@@ -0,0 +1,17 @@
+﻿
+namespace ArenaAPI.DTOs.SubscriptionPlan
+{
+    public class CreateSubscriptionPlanDto
+    {
+        [Required]
+        public string Name { get; set; }
+
+        [Range(1, 24)]
+        public int DurationMonths { get; set; }
+
+        [Range(0.01, 10000)]
+        public decimal Price { get; set; }
+
+        public int SessionLimit { get; set; }
+    }
+}
