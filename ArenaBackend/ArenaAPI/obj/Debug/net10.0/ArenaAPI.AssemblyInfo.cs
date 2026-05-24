7938da6b6e207e75056581e7a1e7ce0bb959274e
rwida <rwidajaber@gmail.com>
2026-05-24 13:18:04 +0300

    Add root .gitignore and remove tracked artifacts

--- ArenaBackend/ArenaAPI/obj/Debug/net10.0/ArenaAPI.AssemblyInfo.cs
+++ ArenaBackend/ArenaAPI/obj/Debug/net10.0/ArenaAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArenaApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da29c781a1afb52ace07a15fd86f9eae5ef14d97")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee8136d472ac1baccc0bbd849c71311c2b4206d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArenaApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArenaApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- ArenaBackend/ArenaApplication/obj/Debug/net10.0/ArenaApplication.AssemblyInfo.cs
+++ ArenaBackend/ArenaApplication/obj/Debug/net10.0/ArenaApplication.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ArenaApplication")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+22803f0f6835c6c2a019e988296ed46b2cc2ed53")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+8ee8136d472ac1baccc0bbd849c71311c2b4206d")]
 [assembly: System.Reflection.AssemblyProductAttribute("ArenaApplication")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ArenaApplication")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
