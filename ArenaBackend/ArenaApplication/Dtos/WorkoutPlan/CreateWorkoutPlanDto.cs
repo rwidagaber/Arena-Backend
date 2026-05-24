88434fd0db7d7dcbab2a807af93d20428efff52b
mohammed eldeeb <mohammedeldeeb550@gmail.com>
2026-05-24 15:57:13 +0300

    create booking and workoutplan dtos

--- ArenaBackend/ArenaApplication/Dtos/WorkoutPlan/CreateWorkoutPlanDto.cs
+++ ArenaBackend/ArenaApplication/Dtos/WorkoutPlan/CreateWorkoutPlanDto.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ArenaApplication.Dtos.WorkoutPlan
 {
-    internal class CreateWorkoutPlanDto
+    public class CreateWorkoutPlanDto
     {
+        [Required]
+        public Guid MemberProfileId { get; set; }
+
+        public Guid? AssignedTrainerId { get; set; }
+
+        [Required]
+        public string Name { get; set; } = string.Empty;
+
+        [Required]
+        public int DurationWeeks { get; set; }
     }
 }
--- ArenaBackend/ArenaApplication/Dtos/WorkoutPlan/UpdateWorkoutPlanDto.cs
+++ ArenaBackend/ArenaApplication/Dtos/WorkoutPlan/UpdateWorkoutPlanDto.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace ArenaApplication.Dtos.WorkoutPlan
 {
-    internal class UpdateWorkoutPlanDto
+    public class UpdateWorkoutPlanDto
     {
+        [Required]
+        public Guid Id { get; set; }
+
+        public Guid? AssignedTrainerId { get; set; }
+
+        public string Name { get; set; } = string.Empty;
+
+        public int DurationWeeks { get; set; }
+
+        public bool IsActive { get; set; }
     }
 }
