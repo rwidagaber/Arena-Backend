25e2a5dffc2e9dee27fd77be60c9f0256bd1689b
khaled ahmed <khaled.pro.963@gmail.com>
2026-05-26 02:27:09 +0300

    Create Payment Dto & IPaymentService

---
+++ ArenaBackend/ArenaApplication/Dtos/Payment/UpdatePaymentStatusDto.cs
@@ -0,0 +1,16 @@
+﻿using ArenaDomain.Enums;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace ArenaApplication.Dtos.Payment
+{
+    public class UpdatePaymentStatusDto
+    {
+        [Required]
+        public PaymentStatus Status { get; set; }
+
+        public string? FailureReason { get; set; }
+    }
+}
---
+++ ArenaBackend/ArenaApplication/IServices/Payment/IPaymentService.cs
@@ -0,0 +1,26 @@
+﻿using ArenaApplication.Dtos.Payment;
+using ArenaDomain.Shared;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ArenaApplication.IServices.Payment
+{
+    public interface IPaymentService
+    {
+        //Member View
+        Task<Result<PaymentDto>> CreateAsync(CreatePaymentDto dto, Guid userId);
+        Task<Result<IEnumerable<PaymentDto>>> GetMyPaymentsAsync(Guid userId);
+
+        Task<Result<PaymentDto>> GetByIdAsync(Guid PaymentId);
+
+        //Admin View
+        Task<Result<IEnumerable<PaymentDto>>> GetAllAsyc(PaymentFilterDto? filter = null);
+
+        //inCash Status
+        Task<Result<PaymentDto>> UpdateStatusAsync(Guid PaymentId, UpdatePaymentStatusDto dto);
+        //webhook
+        Task<Result> MarkAsCompletedAsync(string transactionId, string paymentIntentId);
+        Task<Result> MarkAsFailedAsync(string paymentIntentId, string reason);
+    }
+}
