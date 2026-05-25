93560f6355783873a42d2cabe69745b390410923
Nada Osama <enada1349@gmail.com>
2026-05-25 23:46:32 +0300

    add Jwt Config

--- ArenaBackend/ArenaAPI/Configurations/JWTConfig/JWTConfiguration.cs
+++ ArenaBackend/ArenaAPI/Configurations/JWTConfig/JWTConfiguration.cs
@@ -1,7 +1,46 @@
-﻿namespace ArenaApi.Configurations.JWTConfig
-{
-    public class JWTConfiguration
-    {
+﻿using ArenaDomain.Shared;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using System.Text;
 
+namespace ArenaApi.Configurations.JWTConfig
+{
+    public static class JWTConfiguration
+    {
+        public static IServiceCollection AddJwtAuthentication(
+            this IServiceCollection services,
+            IConfiguration configuration)
+        {
+            services.Configure<JWTSettings>(
+                configuration.GetSection(JWTSettings.SectionName));
+
+            var jwtSettings = configuration
+                .GetSection(JWTSettings.SectionName)
+                .Get<JWTSettings>();
+
+            services
+                .AddAuthentication(options =>
+                {
+                    options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(options =>
+                {
+                    options.TokenValidationParameters = new TokenValidationParameters
+                    {
+                        ValidateIssuer = true,
+                        ValidateAudience = true,
+                        ValidateLifetime = true,
+                        ValidateIssuerSigningKey = true,
+                        ValidIssuer = jwtSettings!.Issuer,
+                        ValidAudience = jwtSettings.Audience,
+                        IssuerSigningKey = new SymmetricSecurityKey(
+                            Encoding.UTF8.GetBytes(jwtSettings.Secret))
+                    };
+                });
+
+            return services;
+        }
     }
 }
+
