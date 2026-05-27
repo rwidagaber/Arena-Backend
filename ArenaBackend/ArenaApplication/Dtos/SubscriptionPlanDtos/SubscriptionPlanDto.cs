1e9634d569ca80be8cc4792b51d3653844d098f6
Wael Al-Bakry <waelbk96@gmail.com>
2026-05-28 02:33:11 +0300

    Change name of folder of SubscriptionPlan Dtos Update the related Dtos

---
+++ ArenaBackend/ArenaApplication/Dtos/SubscriptionPlanDtos/SubscriptionPlanDto.cs
@@ -0,0 +1,12 @@
+﻿namespace ArenaApplication.Dtos.SubscriptionPlanDtos
+{
+    public class SubscriptionPlanDto
+    {
+        public Guid Id { get; set; }
+        public string Name { get; set; }
+        public int DurationMonths { get; set; }
+        public decimal Price { get; set; }
+        public int SessionLimit { get; set; }
+        public bool IsActive { get; set; }
+    }
+}
