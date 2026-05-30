67864e5c32e37d8d205c26c2ea4e21629667387a
khaled ahmed <khaled.pro.963@gmail.com>
2026-05-30 20:47:27 +0300

    chang appSettings

---
+++ ArenaBackend/ArenaApplication/Dtos/Payment/PaymobWebhookDto.cs
@@ -0,0 +1,43 @@
+﻿using System.Text.Json.Serialization;
+
+namespace ArenaApplication.Dtos.Payment
+{
+    public class PaymobWebhookDto
+    {
+        [JsonPropertyName("type")]
+        public string Type { get; set; } = string.Empty;
+
+        [JsonPropertyName("obj")]
+        public PaymobTransactionObj Obj { get; set; } = new();
+    }
+
+    public class PaymobTransactionObj
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }          // TransactionId
+
+        [JsonPropertyName("success")]
+        public bool Success { get; set; }
+
+        [JsonPropertyName("pending")]
+        public bool Pending { get; set; }
+
+        [JsonPropertyName("order")]
+        public PaymobWebhookOrder Order { get; set; } = new();
+
+        [JsonPropertyName("data")]
+        public PaymobWebhookData Data { get; set; } = new();
+    }
+
+    public class PaymobWebhookOrder
+    {
+        [JsonPropertyName("id")]
+        public long Id { get; set; }          // PaymentIntentId
+    }
+
+    public class PaymobWebhookData
+    {
+        [JsonPropertyName("message")]
+        public string Message { get; set; } = string.Empty;
+    }
+}
