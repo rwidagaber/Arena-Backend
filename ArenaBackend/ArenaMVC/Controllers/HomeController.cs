adb4a1584c5a73f02176ff0e2e2c8c92ac860c11
Wael Al-Bakry <waelbk96@gmail.com>
2026-06-03 13:22:35 +0300

    feat: implement dashboard controller and view for admin metrics visualization

--- ArenaBackend/ArenaMVC/Controllers/HomeController.cs
+++ ArenaBackend/ArenaMVC/Controllers/HomeController.cs
@@ -1,13 +1,34 @@
 using System.Diagnostics;
 using Microsoft.AspNetCore.Mvc;
 using ArenaMVC.Models;
+using ArenaApplication.Services.SubscriptionPlan;
 
 namespace ArenaMVC.Controllers;
 
 public class HomeController : Controller
 {
-    public IActionResult Index()
+    private readonly ISubscriptionPlanService _subscriptionPlanService;
+
+    public HomeController(ISubscriptionPlanService subscriptionPlanService)
     {
+        _subscriptionPlanService = subscriptionPlanService;
+    }
+
+    public async Task<IActionResult> Index(CancellationToken cancellationToken)
+    {
+        try
+        {
+            var plans = await _subscriptionPlanService.GetAllAsync(cancellationToken);
+            var plansList = plans.ToList();
+            ViewBag.TotalPlans = plansList.Count;
+            ViewBag.ActivePlans = plansList.Count(p => p.IsActive);
+        }
+        catch
+        {
+            ViewBag.TotalPlans = 0;
+            ViewBag.ActivePlans = 0;
+        }
+
         return View();
     }
 
